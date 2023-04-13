e766807aa7477b01565f64b4a73064e2e38e7e53
Jules <juliaghany@msn.com>
2023-04-13 16:00:53 -0500

    add middleware to server.js file because routes folder with htmlRoutes and apiRoutes were created to clean up code, remove unnecessary packages that were being used before

--- server.js
+++ server.js
@@ -1,58 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const notes = require('./db/db.json');
-const uniqid = require('uniqid');
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // middleware 
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// HTML routes
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-// API routes
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-    // res.json(`${req.method} request received`);
-    // console.log(`${req.method} request received`);
-    return res.json(notes);
-});
-
-// used activity 21 and 22
-app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body
-
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            note_id: uniqid(),
-        };
-
-        const parsedNotes = JSON.parse(data);
-        parsedNotes.push(newNote);
-
-        fs.writeFile('./db/db.json',
-            JSON.stringify(parsedNotes), (writeErr) =>
-            writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully added new note')
-        );
-    }
-});
 
 app.listen(PORT, () =>
     console.info(`Server is running on PORT ${PORT}`)
