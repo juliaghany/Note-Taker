b401de200bd759b2572924563a5abbb4afa2ea0c
Jules <juliaghany@msn.com>
2023-04-13 16:35:55 -0500

    add comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,20 @@
+// import requied modules
+
 const path = require('path');
 const fb = require('express').Router();
+
+// GET route that returns the notes.html file
 
 fb.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// GET route that returns the index.html file
+
 fb.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+// exports fb
 
 module.exports = fb;
